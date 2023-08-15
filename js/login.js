017cf70600a8f80f244aab7c0767e6611c4625a1
yeasin692 <mdyeasin94987@gmail.com>
2023-08-15 15:19:15 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,24 @@
+// step-1: add click event handler with the submit
+document.getElementById('btn-submit').addEventListener('click',function(){
+    // step-2: get email address inside the email input field
+    // always remember to use .value to get text from an input field 
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    //  step-3: get password
+    // 3.a:set id on the html element 
+    // 3.b:get the element 
+    // 3.c:get the value from the element
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(email,password)
+
+    // DANGER: DO NOT VERIFY email password on the client side
+    // step-4: verify email and password and check whether valid user or not
+    if(email === 'sontan@baap.com' && password === 'secret'){
+        window.location.href = 'bank.html';
+    }
+    else{
+        alert('Tui password vule gesos!! Toke ami teijjo sontan gosona korlam');
+        
+    }
+})
